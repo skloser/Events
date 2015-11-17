a92c828dffb2b6460af3a8b70968748beffc56a2
skloser <krum.bagashev@gmail.com>
2015-11-17 10:10:30 +0200

    Just a commit

--- Events/Events.WebApplication/Models/AccountViewModels.cs
+++ Events/Events.WebApplication/Models/AccountViewModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace Events.WebApplication.Models
+﻿namespace Events.WebApplication.Models
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
