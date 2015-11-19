a43d8409c21cb10c1aad5bc5ce2210c6727029e0
orhankj <orhan_kj@abv.bg>
2015-11-19 10:33:32 +0200

    Revert "UnitOfWork + Add Event"
    This reverts commit 3fe22f194f9b405ecd47298589da0f90407d0e1c.

--- Events/Events.Model/Event.cs
+++ Events/Events.Model/Event.cs
@@ -30,8 +30,7 @@
 
         [DataType(DataType.Date)]
         public DateTime StartTime { get; set; }
-
-        [Range(0, int.MaxValue)]
+        
         public int Capacity { get; set; }
 
         public TypeOfEventFormat TypeOfEventFormat { get; set; }
@@ -46,10 +45,6 @@
 
         [MaxLength(200)]
         public string Description { get; set; }
-
-        [Range(0,20)]
-        public int TeamCapacity { get; set; }
-
 
         public virtual ICollection<Team> Teams
         {
