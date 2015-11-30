00689d12364139461413e26a9e4fd84484d8cd5d
skloser <krum.bagashev@gmail.com>
2015-11-30 10:35:56 +0200

    Adding team

---
+++ Events/Events.WebApplication/Controllers/TeamsController.cs
@@ -0,0 +1,34 @@
+﻿using Events.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Events.WebApplication.Controllers
+{
+    public class TeamsController : Controller
+    {
+        // GET: Teams
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateTeam(Team team)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return View(this.ModelState);
+            }
+
+            var teamToAdd = new Team
+            {
+                Name = team.Name,
+            };
+
+            return View();
+        }
+    }
+}
