87f038590e1ffd25a0f77b3831a31da20155198d
Iliyan Ramov <iramov91@gmail.com>
2015-11-30 10:57:22 +0200

    Added events controller

---
+++ Events/Events.WebApplication/Controllers/EventsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Events.Data;
+using Events.Model;
+using Events.Data.Repositories;
+using Events.Model.Statistics;
+
+namespace Events.WebApplication.Controllers
+{
+    public class EventsController : Controller
+    {
+        private EventsDbContext context = new EventsDbContext();
+
+        //public EventsController()
+        //{
+        //    this.context = new UnitOfWork(new EventsDbContext());
+        //}
+
+        //public EventsController(IUnitOfWork context)
+        //{
+        //    this.context = context;
+        //}
+
+        // GET: Events
+        public ActionResult Index()
+        {
+            var events = context.Events.Where(e => e.Host.UserName == User.Identity.Name);
+            
+            return View(events);
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = context.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            //ViewBag.Host = this.User.Identity;
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventId,Title,CreatedOn,StartTime,TeamMembersCapacity,NumberOfTeams,TypeOfEventFormat,TypeOfTeamAssemble,HostId,Description")] Event eventModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                //ViewBag.HostId = new SelectList(context.Users, "Id", "FirstName", @event.HostId);
+                return View(eventModel);
+            }
+            context.Events.Add(eventModel);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = context.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.HostId = new SelectList(context.Users, "Id", "FirstName", @event.HostId);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventId,Title,CreatedOn,StartTime,Capacity,TypeOfEventFormat,TypeOfTeamAssemble,HostId,Description")] Event @event)
+        {
+            
+            
+            if (ModelState.IsValid)
+            {
+                //context.Entry(@event).State = EntityState.Modified;
+                //context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //ViewBag.HostId = new SelectList(context.Users, "Id", "FirstName", @event.HostId);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = context.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = context.Events.Find(id);
+            context.Events.Remove(@event);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
---
+++ Events/Events.WebApplication/Controllers/StatisticsController.cs
@@ -0,0 +1,42 @@
+﻿namespace Events.WebApplication.Controllers
+{
+    using Data;
+    using Model.Statistics;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class StatisticsController : Controller
+    {
+        private EventsDbContext context = new EventsDbContext();
+
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserStatistics()
+        {
+            List<MatchStatistic> userAllMatchStatistics = new List<MatchStatistic>();
+            var userTeams = context.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault().MyTeams;
+            foreach (var userTeam in userTeams)
+            {
+                foreach (var homeGames in userTeam.HomeMatchStatistics)
+                {
+                    userAllMatchStatistics.Add(homeGames);
+                }
+                foreach (var awayGames in userTeam.GuestMatchStatistics)
+                {
+                    userAllMatchStatistics.Add(awayGames);
+                }
+            }
+            var userAllMatchStatisticsOrdered = userAllMatchStatistics.OrderBy(m => m.Event);
+
+            return View(userAllMatchStatisticsOrdered);
+        }
+
+    }
+}
